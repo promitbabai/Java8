4c268686d75fccd707cfe2d7b92ababea5e9c035
Promit Majumder <promitbabai@yahoo.com>
2022-07-27 00:34:24 +0530

    Commit from Eclipse

--- Java8/src/com/test/java8/lamda/Java8LamdaClient.java
+++ Java8/src/com/test/java8/lamda/Java8LamdaClient.java
@@ -15,6 +15,8 @@ public class Java8LamdaClient {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java8");
+		System.out.println("Hello Promit from Java8");
+		System.out.print("Checkin from Eclipse");
 
 	}
 
